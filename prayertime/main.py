9b47a2199a17042bac4d60c74779386cc4ee3332
indkhan <mgsukhan@gmail.com>
2023-09-18 20:59:33 +0300

    making it as function

--- prayertime/main.py
+++ prayertime/main.py
@@ -1,8 +1,23 @@
 import requests
-timesp = requests.get("https://api.aladhan.com/v1/timingsByCity/18-09-2023?city=Jizan&country=Saudi+Arabia&method=4")
-fullt = timesp.json()
-times = fullt["data"]["timings"]
 
-fajr ,fajrEnd ,duhr ,asr ,mag ,isha ,ishaEnd = times["Fajr"] , times["Sunrise"] ,times["Dhuhr"] ,times["Asr"] , times["Maghrib"] , times["Isha"] , times["Midnight"]
+def get_times(city,country):
+    timesp = requests.get(f"https://api.aladhan.com/v1/timingsByCity/18-09-2023?city={city}&country={country}&method=4")
+    fullt = timesp.json()
+    times = fullt["data"]["timings"]
+    #return times
+    fajr ,fajrEnd ,duhr ,asr ,mag ,isha ,ishaEnd = times["Fajr"] , times["Sunrise"] ,times["Dhuhr"] ,times["Asr"] , times["Maghrib"] , times["Isha"] , times["Midnight"]
+    output = (f"fajr starts at : {fajr} \nfajr ends at : {fajrEnd} \nduhr starts at : {duhr} \nasr starts at : {asr} \nmaghrib starts at : {mag} \nisha starts at : {isha} \nisha ends at : {ishaEnd}")
+    return output
 
-print("fajr starts at :  ",fajr ,"\n fahr ends at :  ", fajrEnd ,"\n duhr starts at :  ", duhr ,"\n asr starts at :  ", asr ,"\n magrib starts at :  ", mag ,"\n isha starts at :  ", isha ,"\n isha ends at : " ,ishaEnd)
+def main():
+    
+    city = input("Enter your city : ")
+    country = input("Enter your country : ")
+    print(get_times(city,country))
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
