5c9f8725bd64095765dc6a6acd9ecacec430acaa
indkhan <mgsukhan@gmail.com>
2023-10-14 18:35:03 +0300

    done

---
+++ youtubedown/vidyou.py
@@ -0,0 +1,24 @@
+from pytube import YouTube
+import sys
+import functions
+
+
+try:
+    va = sys.argv[1]
+except:
+    va = input("enter aud or vid :   ")
+try:
+    url = sys.argv[2]
+except:
+    url = input("enter url :   ")
+
+pathvid = r"C:\Users\mgsuk\Videos\Youtube"
+pathaud = r"C:\Users\mgsuk\Music"
+
+if va == "vid":
+    functions.viddown(url,pathvid)
+elif va == "aud":
+    functions.auddown(url,pathaud)
+else:
+    print("choose vid or aud")
+
