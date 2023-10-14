2b7d49833a923dd497c836b3d270cd28df893d12
indkhan <mgsukhan@gmail.com>
2023-10-14 18:17:21 +0300

    making it functional

--- youtubedown/vidyou.py
+++ youtubedown/vidyou.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 import sys
+import functions
 
 
 try:
@@ -11,29 +12,13 @@ try:
 except:
     url = input("enter url :   ")
 
-try:
-    res = sys.argv[3]
-except:
-    res = input("enter resolution :  ")
+pathvid = r"C:\Users\mgsuk\Videos\Youtube"
+pathaud = r"C:\Users\mgsuk\Music"
 
-yt = YouTube(url)
-title = yt.title.split()
-print(title[0])
 if va == "vid":
-    stream = yt.streams.filter(res=res, progressive=True).first()
-
-    out = r"C:\Users\mgsuk\Videos\Youtube"
-    # download video
-    stream.download(output_path=out, filename=f"{title[0]}.mp4")
-
-    print("done")
+    functions.viddown(url,pathvid)
 elif va == "aud":
-    stream = yt.streams.filter(only_audio=True, file_extension='mp4').first()
-
-    out = r"C:\Users\mgsuk\Music"
-    # download video
-    stream.download(output_path=out, filename=f"{title[0]}.mp3")
-
-    print("done")
+    functions.auddown(url,pathaud)
 else:
     print("choose vid or aud")
+
